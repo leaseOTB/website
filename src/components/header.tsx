ee66e24b171d3338e7d0ebfa260bea85c76197ed
Zachary Thielemann <zachdt@gmail.com>
2020-06-30 02:50:30 -0400

    fix: dep

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,7 +19,7 @@ import {
 import theme from "./theme"
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward"
 import useScrollTrigger from "@material-ui/core/useScrollTrigger"
-import { Link } from "gatsby-plugin-intl"
+import { useIntl, FormattedMessage, Link } from "gatsby-plugin-intl"
 
 const Header = (page: any) => {
   return (
