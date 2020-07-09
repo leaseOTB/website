5abbf5282cb027d7c48641915f294b67b9da9671
Joshua Grossman <Josh1794@gmail.com>
2020-07-09 14:24:01 -0400

    fix: added footer component need to fix stylings and links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,98 +7,133 @@ import Language from "./langaugeSelect"
 import theme from "./theme"
 import Platform from "../pages/platform"
 import { useIntl, FormattedMessage, Link } from "gatsby-plugin-intl"
+import { Footer } from "@leaseOTB/ui-components"
 
-const Footer = () => {
-  const intl = useIntl()
+const FooterComponent = () => {
+  const data = [
+    {
+      title: "About",
+      items: [
+        { item: "item 1", link: "https://leaseontheblock.care" },
+        { item: "item 2", link: "https://google.com" },
+      ],
+    },
+    {
+      title: "Platform",
+      items: [
+        { item: "item 1", link: "https://leaseontheblock.care" },
+        { item: "item 2", link: "https://google.com" },
+      ],
+    },
+    {
+      title: "Team",
+      items: [
+        { item: "item 1", link: "https://leaseontheblock.care" },
+        { item: "item 2", link: "https://google.com" },
+      ],
+    },
+  ]
+
   return (
-    <ThemeProvider theme={theme}>
-      <div
-        style={{
-          minHeight: "4em",
-          backgroundColor: theme.palette.secondary.main,
-          color: theme.palette.secondary.light,
-          marginTop: "2em",
-          padding: "1em 1em .5em 2em",
-          borderRadius: "1em",
-          boxShadow: `150px 10px ${theme.palette.primary.main}`,
-        }}
-      >
-        <Grid container justify="space-between" alignItems="center">
-          <Grid item xs={12} sm={7}>
-            <Grid container direction="row" justify="flex-start">
-            <Link
-                to="/about"
-                style={{
-                  textDecoration: "none",
-                  color: theme.palette.secondary.light,
-                  marginRight: "2em",
-                }}
-              >
-                <Typography variant="body1">
-                  <FormattedMessage id="aboutButton" />
-                </Typography>
-              </Link>
-              <Link
-                to="/platform"
-                style={{
-                  textDecoration: "none",
-                  color: theme.palette.secondary.light,
-                  marginRight: "2em",
-                }}
-              >
-                <Typography variant="body1">
-                  <FormattedMessage id="platformButton" />
-                </Typography>
-              </Link>
-              <Link
-                to="/team"
-                style={{
-                  textDecoration: "none",
-                  color: theme.palette.secondary.light,
-                  marginRight: "2em",
-                }}
-              >
-                <Typography variant="body1">
-                  <FormattedMessage id="teamButton" />
-                </Typography>
-              </Link>
-            </Grid>
-            <Grid item xs={11}>
-              <Language />
-            </Grid> 
-            <Typography variant="body2" color="primary">
-              © {new Date().getFullYear()}{" "}
-              <FormattedMessage id="copyrightText" />
-            </Typography>
-          </Grid>
-          <FormattedMessage id="socialText" />
-          <Grid item xs={12} sm={3}>
-            <IconButton
-              style={{ color: theme.palette.secondary.light }}
-              target="__blank"
-              href="https://twitter.com/LeaseOTB"
-            >
-              <TwitterIcon />
-            </IconButton>
-            <IconButton
-              style={{ color: theme.palette.secondary.light }}
-              target="__blank"
-              href="https://github.com/leaseOTB"
-            >
-              <GitHubIcon />
-            </IconButton>
-            <IconButton
-              style={{ color: theme.palette.secondary.light }}
-              target="__blank"
-              href="https://www.linkedin.com/company/43276219"
-            >
-              <LinkedInIcon />
-            </IconButton>
-          </Grid>
-        </Grid>
-      </div>
-    </ThemeProvider>
+    <Footer
+      style={{
+        width: "10px",
+      }}
+      props={data}
+    />
   )
 }
 
-export default Footer
+// OLD FOOTER CODE
+
+// const intl = useIntl()
+// return (
+//   <ThemeProvider theme={theme}>
+//     <div
+//       style={{
+//         minHeight: "4em",
+//         backgroundColor: theme.palette.secondary.main,
+//         color: theme.palette.secondary.light,
+//         marginTop: "2em",
+//         padding: "1em 1em .5em 2em",
+//         borderRadius: "1em",
+//         boxShadow: `150px 10px ${theme.palette.primary.main}`,
+//       }}
+//     >
+//       <Grid container justify="space-between" alignItems="center">
+//         <Grid item xs={12} sm={7}>
+//           <Grid container direction="row" justify="flex-start">
+//             <Link
+//               to="/about"
+//               style={{
+//                 textDecoration: "none",
+//                 color: theme.palette.secondary.light,
+//                 marginRight: "2em",
+//               }}
+//             >
+//               <Typography variant="body1">
+//                 <FormattedMessage id="aboutButton" />
+//               </Typography>
+//             </Link>
+//             <Link
+//               to="/platform"
+//               style={{
+//                 textDecoration: "none",
+//                 color: theme.palette.secondary.light,
+//                 marginRight: "2em",
+//               }}
+//             >
+//               <Typography variant="body1">
+//                 <FormattedMessage id="platformButton" />
+//               </Typography>
+//             </Link>
+//             <Link
+//               to="/team"
+//               style={{
+//                 textDecoration: "none",
+//                 color: theme.palette.secondary.light,
+//                 marginRight: "2em",
+//               }}
+//             >
+//               <Typography variant="body1">
+//                 <FormattedMessage id="teamButton" />
+//               </Typography>
+//             </Link>
+//           </Grid>
+//           <Grid item xs={11}>
+//             <Language />
+//           </Grid>
+//           <Typography variant="body2" color="primary">
+//             © {new Date().getFullYear()}{" "}
+//             <FormattedMessage id="copyrightText" />
+//           </Typography>
+//         </Grid>
+//         <FormattedMessage id="socialText" />
+//         <Grid item xs={12} sm={3}>
+//           <IconButton
+//             style={{ color: theme.palette.secondary.light }}
+//             target="__blank"
+//             href="https://twitter.com/LeaseOTB"
+//           >
+//             <TwitterIcon />
+//           </IconButton>
+//           <IconButton
+//             style={{ color: theme.palette.secondary.light }}
+//             target="__blank"
+//             href="https://github.com/leaseOTB"
+//           >
+//             <GitHubIcon />
+//           </IconButton>
+//           <IconButton
+//             style={{ color: theme.palette.secondary.light }}
+//             target="__blank"
+//             href="https://www.linkedin.com/company/43276219"
+//           >
+//             <LinkedInIcon />
+//           </IconButton>
+//         </Grid>
+//       </Grid>
+//     </div>
+//   </ThemeProvider>
+
+export default FooterComponent
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
-import Footer from "./footer"
+import FooterComponent from "./footer"
 import "./layout.css"
 import theme from "./theme"
 import ScrollUpButton from "react-scroll-up-button" //Add this line Here
@@ -48,8 +48,8 @@ const Layout = ({ children, path }) => {
         }}
       >
         <main>{children}</main>
-        <Footer />
       </div>
+      <FooterComponent />
     </div>
   )
 }
